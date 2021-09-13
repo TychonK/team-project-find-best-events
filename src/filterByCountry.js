d2253ce617e1ff7d3923a4c9df2604f24c67ec05
AleksDu <aleksandr.dyshkin@gmail.com>
2021-09-13 17:21:21 +0300

    Update filterByCountry.js

--- src/filterByCountry.js
+++ src/filterByCountry.js
@@ -1,18 +1,20 @@
-import template from '../templates/option.hbs';
+import template from '../src/templates/option.hbs';
 export default {
   API_KEY: 'PLEluArGwTZQl36ty5ijCNPhmvtWXv1M',
   language: 'en-US',
-async getCountries() {
+  async getCountries() {
     const response = await fetch("https://app.ticketmaster.com/discovery/v2/json?country&apikey={apikey}");
-     if (response.ok) {
+    if (response.ok) {
       return await response.json();
     }
+  }
+}
 
 
     let filters = document.querySelector('.filters');
     let select = document.querySelector('.select-js');
 
-    async function getCountries() {
+     async function getCountries() {
         const countries = await fetchApi.getCountries();
         return countries;
     }
@@ -51,13 +53,13 @@ async function onFilterChooseAndRenderPages(e) {
     main.dataset.page = 'filtering';
     // renderByCountriFilter(e.target.value, 1);
 
-    export function onHomeClickHandler() {
+    function onHomeClickHandler() {
   filters.classList.remove('visually-hidden');
   status = 'home';
 }
-export function onMyLibraryClickHandler() {
+ function onMyLibraryClickHandler() {
     status = 'library';
     filters.classList.add('visually-hidden');
 }
 
-export {getCountries}
+export { getCountries };
