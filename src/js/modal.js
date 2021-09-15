4891c6f043ec9a213977d067a5b5287172c9cd89
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-09-16 00:19:02 +0200

    добавляет ф-ю поиска ивента по id

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,28 +1,58 @@
 
-// добавить, когда будет разметка
-// function onImageOpenClick(event) {
-//   event.preventDefault();
-//   if(!event.target.classList.contains('gallery__image')){
-// return
-//   }
-  
-//   onOpenModal();
+const refs = {
+    modalContainer: document.querySelector(".modal"),
+    modalImg: document.querySelector('.modal__image'),
+    modalOpenBtn: document.querySelector('.open-btn'),
+    modalCloseBtn: document.querySelector('.modal__button'),
+  modalOverlay: document.querySelector('.modal__overlay'),
+    eventsGallery: document.querySelector('.events')
+};
+
+let eventModalSrc = '';
+
+
+refs.eventsGallery.addEventListener('click', onEventOpenClick);
+function onEventOpenClick(event) {
+  event.preventDefault();
+  if (!event.target.classList === 'gallery__image'
+  ) {
+    console.log('мимо');
+return
+  }
+  eventModalSrc = event.target.dataset.src;
+  // console.log(event.target.dataset.src);
+  // console.log(eventModalSrc);
+  onOpenModal();
+  createModalContent(eventModalSrc)
+  // console.log(event);
+  // console.log(event.target.dataset.src);
+ 
+ 
+  // console.log(event.dataset);
+  // console.log(event.target);
+  // console.log(event.target.data-src.value);
+  // console.log(event.target.src);
 
 // const modalImgLink = event.target.dataset.source;
 // const modalImgAlt = event.target.alt;
 
 // modalImg.alt = modalImgAlt;
 // modalImg.src = modalImgLink;
-// }
+}
+
+function createModalContent(eventModalSrc) {
+     
+    return fetch( `https://app.ticketmaster.com/discovery/v2/events.json?id=${eventModalSrc}&apikey=PLEluArGwTZQl36ty5ijCNPhmvtWXv1M`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+      
+        .catch(err => {
+            console.log(err);
+            // error ({ text: 'No results' })
+        })
+    };
 
 
-const refs = {
-    modalContainer: document.querySelector(".modal"),
-    modalImg: document.querySelector('.modal__image'),
-    modalOpenBtn: document.querySelector('.open-btn'),
-    modalCloseBtn: document.querySelector('.modal__button'),
-    modalOverlay: document.querySelector('.modal__overlay')
-};
 
 refs.modalOpenBtn.addEventListener('click', onOpenModal);
 
