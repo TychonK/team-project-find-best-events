c27484c1ee07648a2ecd2e4cb279c183cb96ec04
Alona-Isko <79259446+Alona-Isko@users.noreply.github.com>
2021-09-23 17:38:24 +0200

    pnotify

--- src/js/pnotify.js
+++ src/js/pnotify.js
@@ -4,17 +4,20 @@ import '@pnotify/core/dist/BrightTheme.css';
 
 export const PNotifyEmptyInput = () => alert({
     text: 'Please enter something!',
-    delay: 2000,
+  delay: 2000,
+    width: '280px',
 });
 
 export const PNotifyError = () => error({
   title: 'Error!',
   text: 'This query does not exist!',
-  delay: 2000
+  delay: 2000,
+  width: '280px',
 });
 
 export const PNotifyForCountry = () => info({
   text: 'Sorry, but we haven not found any events for your request',
-  delay: 2000
+  delay: 2000,
+  width: '280px',
 });
 
